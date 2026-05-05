a58c04a544404383ed7568f3a412553287fe25a1
Traffik Boosters <traffikboosters@gmail.com>
2026-05-05 15:19:04 -0400

    redirect home to starz-os

--- starz-os-frontend-v2/app/page.tsx
+++ starz-os-frontend-v2/app/page.tsx
@@ -1,1 +1,1 @@
-import { redirect } from "next/navigation"; export default function Home() { redirect("/leads"); }
+import { redirect } from "next/navigation"; export default function Home() { redirect("/starz-os.html"); }
